52f2bd724c37817ce226fa895a478ae5a3f043a7
ekta <aektapatel0102@gmail.com>
2022-07-28 10:07:09 +0100

    my java learning program

---
+++ IfStatements.java
@@ -0,0 +1,18 @@
+package If;
+
+public class IfStatements {
+    public static void main(String[] args) {
+        int age = 22;
+        if (age >= 18) {
+            System.out.println("candidate is eligible for voting");
+        }
+        int canage =13;
+        if(canage >=18) {
+            System.out.println("candidate is eligible for voting");
+        }
+        else{
+             System.out.println("candidate is not eligible for voting");
+        }
+
+    }
+}
---
+++ NumPositiveNegative.java
@@ -0,0 +1,18 @@
+package If;
+
+import sun.awt.geom.AreaOp;
+
+public class NumPositiveNegative {
+    public static void main(String[] args) {
+        int n =2;
+        if(n>=0) {
+            System.out.println("number is positive");
+        }
+        int j =-4;
+        if(j>=0) {
+            System.out.println("number is positive");
+        }else{
+            System.out.println("number is negative");
+        }
+    }
+}
